fdc377adf4de2a6d2e024b93c9a4dc42b5a440c5
Henrique Bispo <henrique@obispo.com.br>
2025-12-12 17:28:08 -0300

    Mobile

--- resources/views/meals/index.blade.php
+++ resources/views/meals/index.blade.php
@@ -22,7 +22,7 @@
 
         <div class="mb-8 flex gap-4">
             <a href="{{ route('meals.create') }}" class="inline-block px-5 py-2 bg-[#1b1b18] dark:bg-[#eeeeec] text-white dark:text-[#1C1C1A] rounded-sm hover:bg-black dark:hover:bg-white transition">
-                + Meal
+                +
             </a>
             <a href="{{ route('meals.list') }}" class="inline-block px-5 py-2 border border-[#e3e3e0] dark:border-[#3E3E3A] rounded-sm hover:border-[#19140035] dark:hover:border-[#62605b] transition">
                 Meals
